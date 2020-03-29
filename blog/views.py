c59882fcbc74c984800ddb4beadad62494eaebbb
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-30 03:35:31 +0600

    author posts added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.views import generic
 from .models import Post
 
@@ -10,3 +11,18 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'posts/detail.html'
+
+
+class AuthorPosts(generic.ListView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        author_id = self.kwargs['author_id']
+        author = User.objects.get(id=author_id)
+        context = super().get_context_data(**kwargs)
+        context['author'] = author
+        context['author_posts'] = Post.objects.filter(author=author)
+        return context
+
+    template_name = 'posts/author.html'
+
