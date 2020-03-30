62ca1183a5ecd8574191515cd5e63848b27e2ac0
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-31 01:47:34 +0600

    path name format update

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='posts.index'),
-    path('posts/<int:pk>', views.PostDetail.as_view(), name='posts.detail'),
-    path('author/<int:author_id>/posts', views.AuthorPosts.as_view(), name='author.posts'),
+    path('', views.PostList.as_view(), name='posts-index'),
+    path('posts/<int:pk>', views.PostDetail.as_view(), name='posts-detail'),
+    path('author/<int:author_id>/posts', views.AuthorPosts.as_view(), name='author-posts'),
 ]
